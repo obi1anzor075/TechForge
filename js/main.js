934974f876421befff65b5fa84ba40f0ca4af015
obi1anzor075 <shindin112@gmail.com>
2024-03-13 11:32:54 +0300

    add logic send form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-})
+function sendEmail() {
+
+  var name = document.getElementsByName("name")[0].value;
+
+  var email = document.getElementsByName("email")[0].value;
+
+  var message = document.getElementsByName("message")[0].value;
+
+  var subject = "Новое сообщение от " + name;
+
+  var body = "Имя: " + name + "
+
+  ";
+
+  body += "Email: " + email + "
+
+  ";
+
+  body += "Сообщение:
+
+  " + message;
+
+  window.open("mailto:youremail@example.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body));
+}
